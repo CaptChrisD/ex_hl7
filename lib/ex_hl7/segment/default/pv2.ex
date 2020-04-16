deb74782e02e0070b0335907b5c185f6b54be6b9
Chris Dutton <chris@aquatic-edge.com>
2020-04-16 09:52:14 -0700

    Add visit admit id and reason to PV2

--- lib/ex_hl7/segment/default/pv2.ex
+++ lib/ex_hl7/segment/default/pv2.ex
@@ -5,6 +5,8 @@ defmodule HL7.Segment.Default.PV2 do
   require HL7.Composite.Default.CE, as: CE
 
   segment "PV2" do
+    field :admit_reason_id,    seq: 3, type: {CE, :id}, len: 20
+    field :admit_reason_text,  seq: 3, type: {CE, :text}, len: 199
     field :transfer_reason_id, seq: 4, type: {CE, :id}, len: 20
   end
 end
